39db33af14485e2c1797d537bdf0008d5adc7c2e
zhenya <eee@i.ee>
2020-08-31 21:05:03 +0300

    devochka

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
        Human hum=new Human("Serezha",9,1.75,65.8);
        hum.say();
+       Human girl1 = new Girl("Nastya",18,180,60);
+       girl1.say();
         }
 }
 
