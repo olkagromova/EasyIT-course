a7048e9f65d7f6537e48a0de5a1b853e651416de
zhenya <eee@i.ee>
2020-08-31 19:57:58 +0300

    first commit

---
+++ src/com/company/task2/Human.java
@@ -0,0 +1,22 @@
+package com.company.task2;
+
+public class Human {
+    public String name;
+    public int age;
+    public double height;
+    public double weitht;
+
+    public Human(String name,int age,double height,double weitht){
+        this.name=name;
+        this.age=age;
+        this.height=height;
+        this.weitht=weitht;
+    }
+
+    public void say() {
+        System.out.println("Name: "+name+"\n"+" Age: "+age+"\n"+" Height: "+height+"\n"+" Weitht: "+weitht);
+    }
+
+}
+
+
---
+++ src/com/company/task2/Main.java
@@ -0,0 +1,12 @@
+package com.company.task2;
+
+import com.company.task2.Human;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Human human=new Human("Dima",13,1.75,65.8);
+       human.say();
+        }
+}
+
