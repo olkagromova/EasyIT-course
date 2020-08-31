a9105ed931bdb1e9d48b915cbcb14a33155bcf91
zhenya <eee@i.ee>
2020-08-31 20:36:46 +0300

    third_commit

--- src/com/company/task2/Human.java
+++ src/com/company/task2/Human.java
@@ -1,10 +1,10 @@
 package com.company.task2;
 
 public class Human {
-    public String name;
-    public int age;
-    public double height;
-    public double weitht;
+    private String name;
+    private int age;
+    private double height;
+    private double weitht;
 
     public Human(String name,int age,double height,double weitht){
         this.name=name;
