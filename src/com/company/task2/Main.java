2708d0236a1f12f278949fc58dae6f9332de5837
zhenya <eee@i.ee>
2020-08-31 20:16:54 +0300

    Edit_name

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -5,8 +5,8 @@ import com.company.task2.Human;
 public class Main {
 
     public static void main(String[] args) {
-       Human human=new Human("Serezha",9,1.75,65.8);
-       human.say();
+       Human hum=new Human("Serezha",9,1.75,65.8);
+       hum.say();
         }
 }
 
