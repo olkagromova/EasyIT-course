2b2febd4d22510e43d957fe88dcfc8a37cd9c786
user <olkgromova@gmail.com>
2020-09-02 21:19:39 +0300

    add Boy

--- src/com/company/task2/Boy.java
+++ src/com/company/task2/Boy.java
@@ -1,4 +1,13 @@
 package com.company.task2;
 
-public class Boy {
+public class Boy extends  Human{
+    public Boy(String name, int age, double height, double weitht) {
+        super(name, age, height, weitht);
+
+    }
+
+    @Override
+    public void say() {
+        super.say();
+    }
 }
